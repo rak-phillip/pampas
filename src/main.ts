22882fcf89141272f2292454e88ee778cffe07a8
Phillip Rak <rak.phillip@gmail.com>
2023-11-07 15:59:06 -0700

    Configure font-awesome
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,14 @@ import './assets/main.css';
 
 import { createApp } from 'vue';
 import App from './App.vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faSort, faSortUp, faSortDown } from '@fortawesome/free-solid-svg-icons';
 
-createApp(App).mount('#app');
+library.add(faSort);
+library.add(faSortUp);
+library.add(faSortDown);
+
+createApp(App)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .mount('#app');
